0c584938c534b512d05ebcb2d9e73eb72568e7a4
marydong <mary_dong@brown.edu>
2019-05-10 18:48:41 -0400

    we're done

--- src/main.py
+++ src/main.py
@@ -2,18 +2,16 @@ from img_preprocessing import crop_all_center, get_time_slices
 
 def main():
 
-    # small video of road
-    # crop_all_center('data/video/vid1_short', 'data/video/vid1_short_cropped', 300, 100)
-    # crop_all_center('data/video/vid1_short', 'data/video/vid1_short_cropped', 300, 100)
-	#
-    # # larger video of road
-    # crop_all_center('data/video/vid1_short', 'data/video/vid1_round2_cropped', 600, 200)
+    # small video of road (we ended up not using this in our final results)
+    # crop_all_center('data/application_video/vid1_short', 'data/application_video/vid1_short_cropped', 300, 100)
+    # crop_all_center('data/application_video/vid1_short', 'data/application_video/vid1_short_cropped', 300, 100)
+    # crop_all_center('data/application_video/vid1_short', 'data/application_video/vid1_round2_cropped', 600, 200)
 
     # get time slices from a directory of low rate frames
-    # get_time_slices('data/video/low_fps', 'data/video/time_slices')
+    # get_time_slices('data/application_video/time/low_fps_frames', 'data/application_video/time/time_slices_original')
 
     # uncomment this and change to slice on second dimension in array ('dim1') to get new upsampled frames
-    get_time_slices('data/video/time_upsample_x4', 'data/video/high_fps_x4')
+    # get_time_slices('data/application_video/time/time_slice_upsample_x4', 'data/application_video/time/high_fps_frame_x4')
 
 
 if __name__ == "__main__":
